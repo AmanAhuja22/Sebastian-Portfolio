23a0000773fc5be189e55519b3920e63c1b1de23
0x-Sarthak <sarthakahuja447@gmail.com>
2022-09-21 23:50:34 +0530

    Youtube Videos Added

--- components/Tutorials/index.js
+++ components/Tutorials/index.js
@@ -2,8 +2,31 @@ import React from "react";
 
 const Tutorial = () => {
 	return (
-		<div className="purple-gradient text-white text-center h-80 text-4xl pt-12">
-			Tutorials
+		<div className="purple-gradient text-white text-center  text-4xl pt-12">
+			<h1>Tutorials</h1>
+
+			<div className="flex  justify-center items-center">
+				<div className="p-12">
+					<iframe
+						width="560"
+						height="315"
+						src="https://www.youtube.com/embed/shCTEepchps"
+						title="YouTube video player"
+						frameborder="0"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+						allowfullscreen></iframe>
+				</div>
+				<div className="p-12">
+					<iframe
+						width="560"
+						height="315"
+						src="https://www.youtube.com/embed/9INsrElC0pU"
+						title="YouTube video player"
+						frameborder="0"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+						allowfullscreen></iframe>
+				</div>
+			</div>
 		</div>
 	);
 };
