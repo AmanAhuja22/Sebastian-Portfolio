4656438600e47f0a94c65876080136ba6b059c89
0x-Sarthak <sarthakahuja447@gmail.com>
2022-09-21 20:34:23 +0530

    Logo's Added

--- components/Support/index.js
+++ components/Support/index.js
@@ -3,47 +3,59 @@ import Image from "next/image";
 import Revealed from "../../public/images/Revealed.png";
 import Afrojack from "../../public/images/Afrojack.svg";
 import Hardwell from "../../public/images/Hardwell.svg";
-import MartinGarrix from "../../public/images/MartinGarrix.svg";
 import David from "../../public/images/David.png";
-import DavidGuetta from "../../public/images/DavidGuetta.svg";
+import DonDiable from "../../public/images/DonDiable.png";
+import Hexagon from "../../public/images/Hexagon.png";
+
 const Support = () => {
 	return (
 		<div className="h-full font-main text-white purple-gradient text-center  text-4xl pt-12">
 			<div className="p-4">Supported By</div>
-			{/* <div className="w-18">
-				<Image width="90px" height="90px" src={Revealed} alt="Revealed" />
-			</div> */}
 
-			{/* <div className="">
-				<Image
-					style={{ background: "white" }}
-					width="280px"
-					height="50px"
-					src={DavidGuetta}
-				/>
-			</div> */}
-			<div className="w-18">
-				<Image src={Afrojack} alt="Afrojack" />
+			<div className="flex justify-center items-center gap-24 mt-12">
+				<div className="w-18 cursor-pointer" title="Afrojack">
+					<Image src={Afrojack} alt="Afrojack" />
+				</div>
+				<div className="w-18 cursor-pointer" title="David Guetta">
+					<Image width="200" height={28} src={David} alt="David" />
+				</div>
+				<div className="w-18 cursor-pointer" title="Hardwell">
+					<Image
+						style={{ background: "white" }}
+						width="180px"
+						height="50px"
+						src={Hardwell}
+					/>
+				</div>
+				<div className="w-18">
+					<Image width="200px" height="40px" src={Hexagon} />
+				</div>
+				<div className="w-18" title="Revealed Recordings">
+					<Image width="90px" height="90px" src={Revealed} alt="Revealed" />
+				</div>
+				{/* <div className="w-18">
+					<Image width="200px" height="80px" src={DonDiable} />
+				</div> */}
 			</div>
-			<div className="w-18">
-				<Image width="200" height={28} src={David} alt="David" />
+			<div className="flex justify-center items-center gap-24 mt-12">
+				<div className="w-18 cursor-pointer" title="Hardwell">
+					<Image
+						style={{ background: "white" }}
+						width="180px"
+						height="50px"
+						src={Hardwell}
+					/>
+				</div>
+				<div className="w-18">
+					<Image width="200px" height="40px" src={Hexagon} />
+				</div>
+				<div className="w-18" title="Revealed Recordings">
+					<Image width="90px" height="90px" src={Revealed} alt="Revealed" />
+				</div>
+				<div className="w-18">
+					<Image width="200px" height="80px" src={DonDiable} />
+				</div>
 			</div>
-			{/* <div className="w-18">
-				<Image
-					style={{ background: "white" }}
-					width="180px"
-					height="50px"
-					src={Hardwell}
-				/>
-			</div> */}
-			{/* <div className="w-18">
-				<Image
-					style={{ background: "white" }}
-					width="180px"
-					height="50px"
-					src={MartinGarrix}
-				/>
-			</div> */}
 		</div>
 	);
 };
