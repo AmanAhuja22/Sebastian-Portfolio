52970e51e8bac6f196fdf6237bbac28ab030e864
0x-Sarthak <sarthakahuja447@gmail.com>
2022-09-22 22:56:05 +0530

    Youtube Fade

--- components/Tutorials/index.js
+++ components/Tutorials/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Fade } from "react-reveal";
 
 const Tutorial = () => {
 	return (
@@ -6,26 +7,30 @@ const Tutorial = () => {
 			<h1>Tutorials</h1>
 
 			<div className="lg:flex sm:flex-row justify-center items-center">
-				<div className="p-12">
-					<iframe
-						width="560"
-						height="315"
-						src="https://www.youtube.com/embed/shCTEepchps"
-						title="YouTube video player"
-						frameBorder="0"
-						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-						allowFullScreen></iframe>
-				</div>
-				<div className="p-12">
-					<iframe
-						width="560"
-						height="315"
-						src="https://www.youtube.com/embed/9INsrElC0pU"
-						title="YouTube video player"
-						frameBorder="0"
-						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-						allowFullScreen></iframe>
-				</div>
+				<Fade left>
+					<div className="p-12">
+						<iframe
+							width="560"
+							height="315"
+							src="https://www.youtube.com/embed/shCTEepchps"
+							title="YouTube video player"
+							frameBorder="0"
+							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+							allowFullScreen></iframe>
+					</div>
+				</Fade>
+				<Fade right>
+					<div className="p-12">
+						<iframe
+							width="560"
+							height="315"
+							src="https://www.youtube.com/embed/9INsrElC0pU"
+							title="YouTube video player"
+							frameBorder="0"
+							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+							allowFullScreen></iframe>
+					</div>
+				</Fade>
 			</div>
 		</div>
 	);
